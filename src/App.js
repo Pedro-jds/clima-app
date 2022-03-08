7abaa6ac6e0b74367fc7ad2e42340f4d7736ab80
Pedro jr <pedro81565@hotmail.com.com>
2022-03-08 13:38:21 -0300

    limpeza de arquivos e criação do clima-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=pt_br&appid=6a25c2f0385ec75637f083ba80e6b12a`;
+
+  const searchLocation = (event) => {
+    if(event.key==="Enter"){
+      axios.get(url).then((res)=>{
+        setData(res.data)
+        console.log(res.data)
+      })
+      setLocation('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search"> 
+        <input
+        value={location}
+        onChange={ev =>setLocation(ev.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Digite a localização'
+        type='text'
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main.temp/10)}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather? <p>{data.weather[0].description}</p> : null}
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p>{Math.round(data.main.feels_like/10)}°C</p> : null}
+            <p>Sensação</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p>{data.main.humidity}%</p> : null}
+            <p>Umidade</p>
+          </div>
+          <div className="wind">
+            <p>{}km</p>
+            <p>Vento</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
